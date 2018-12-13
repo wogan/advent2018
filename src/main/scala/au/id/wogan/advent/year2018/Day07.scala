b9bd5887e03e5f8eda92807a585f88df7abbb998
gbrowne <gbrowne@marchex.com>
2018-12-13 08:38:36 -0800

    Day 7 part one

--- src/main/scala/au/id/wogan/advent/year2018/Day07.scala
+++ src/main/scala/au/id/wogan/advent/year2018/Day07.scala
@@ -1,5 +1,8 @@
 package au.id.wogan.advent.year2018
 
+import scala.annotation.tailrec
+import scala.util.matching.Regex
+
 object Day07 extends AdventApp(7) {
 
   val testInput: Vector[String] =
@@ -11,7 +14,49 @@ object Day07 extends AdventApp(7) {
       |Step D must be finished before step E can begin.
       |Step F must be finished before step E can begin.""".testInput
 
-  override def go(): Unit = {
+  val regex: Regex = "Step (?<step>.+) must be finished before step (?<dep>.+) can begin.".r
 
+  def parse(line: String): (String, String) = {
+    implicit val m: Regex.Match = regex.findFirstMatchIn(line).get
+    ('step, 'dep)
+  }
+
+  case class Step(name: String, prequisites: Set[String]) {
+    def isAvailable(completed: Set[String]): Boolean =
+      (prequisites -- completed).isEmpty
+
+    def duration: Int =
+      name.head - 'A' + 1
+  }
+
+  type Worker = Option[(String, Int)]
+
+  override def go(): Unit = {
+    val data = testInput map parse foldMap {
+      case (step, dep) =>
+        Map(step -> Set.empty[String], dep -> Set(step))
+    } map (Step.apply _).tupled
+
+    val (letter, _) = findOrder(List.empty, data.toSet)
+    println(s"Part One: ${letter.mkString("")}")
+
+
+  }
+
+  def findOrderWithTime(time: Int, completed: Set[String], workers: List[Worker], remaining: Set[Step]): (Int, Set[String], List[Worker], Set[Step]) =
+  {
+    val available = remaining.filter(_.isAvailable(completed))
+
+  }
+
+  @tailrec
+  def findOrder(completed: List[String], remaining: Set[Step]): (List[String], Set[Step]) = {
+    val set = completed.toSet
+    val toRemove = remaining.filter(_.isAvailable(set)).minBy(_.name)
+    val rest = remaining - toRemove
+    if (rest.isEmpty)
+      (completed :+ toRemove.name) -> rest
+    else
+      findOrder(completed :+ toRemove.name, rest)
   }
 }

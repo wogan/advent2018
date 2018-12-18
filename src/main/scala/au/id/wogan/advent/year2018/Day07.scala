ecaf924abf001811a0a65d9321ef1dae41899cd8
Greg Wogan-Browne <gbrowne@marchex.com>
2018-12-18 15:52:35 -0800

    Completed day 7

--- src/main/scala/au/id/wogan/advent/year2018/Day07.scala
+++ src/main/scala/au/id/wogan/advent/year2018/Day07.scala
@@ -21,37 +21,82 @@ object Day07 extends AdventApp(7) {
     ('step, 'dep)
   }
 
-  case class Step(name: String, prequisites: Set[String]) {
+  case class Step(name: String, prerequisites: Set[String]) {
     def isAvailable(completed: Set[String]): Boolean =
-      (prequisites -- completed).isEmpty
+      (prerequisites -- completed).isEmpty
 
     def duration: Int =
-      name.head - 'A' + 1
+      name.head - 'A' + 61
   }
 
   type Worker = Option[(String, Int)]
 
   override def go(): Unit = {
-    val data = testInput map parse foldMap {
+    val data = input map parse foldMap {
       case (step, dep) =>
         Map(step -> Set.empty[String], dep -> Set(step))
     } map (Step.apply _).tupled
 
     val (letter, _) = findOrder(List.empty, data.toSet)
     println(s"Part One: ${letter.mkString("")}")
-
-
+    val state = findOrderWithTime(State(data.toSet))
+    println(s"Part Two: ${state.time}")
   }
 
-  def findOrderWithTime(time: Int, completed: Set[String], workers: List[Worker], remaining: Set[Step]): (Int, Set[String], List[Worker], Set[Step]) = {
-    val available = remaining.filter(_.isAvailable(completed))
-    val updatedWorkers = workers map {
-      case Some((_, t)) if t <= time =>
-        None
-      case other =>
-        other
+  case class State(workers: List[Worker], remaining: Set[Step], time: Int, completed: Set[String])
+  object State {
+    def apply(remaining: Set[Step], workerCount: Int = 5): State =
+      new State(List.fill(workerCount)(None), remaining, 0, Set.empty)
+  }
+
+  @tailrec
+  def findOrderWithTime(input: State): State = {
+    import input._
+    val updatedWorkers = workers.updateFor(time)
+    val newCompleted = completed ++ workers.completed(time)
+    if (updatedWorkers.idle && remaining.isEmpty) {
+      input.copy(workers = updatedWorkers, completed = newCompleted)      // we're done!
+    } else {
+      val availableSteps = remaining.filter(_.isAvailable(newCompleted))
+      val (newWorkers, takenWork) = updatedWorkers.perform(availableSteps, time)
+      val newState = input.copy(
+        completed = newCompleted,
+        workers = newWorkers,
+        time = newWorkers.nextTime,
+        remaining = remaining -- takenWork)
+      findOrderWithTime(newState)
     }
-    ???
+  }
+
+  implicit class WorkerMethods(workers: List[Worker]) {
+    def updateFor(time: Int): List[Worker] =
+      workers map {
+        case Some((_, t)) if t <= time =>
+          None
+        case other =>
+          other
+      }
+    def completed(time: Int): List[String] =
+      workers collect {
+        case Some((name, t)) if t <= time =>
+          name
+      }
+    def idle: Boolean =
+      workers.forall(_.isEmpty)
+    def perform(steps: Set[Step], time: Int): (List[Worker], Set[Step]) = {
+      val ordered = steps.toList.sortBy(_.name)
+      val (working, idle) = workers.partition(_.isDefined)
+      val s = Math.min(idle.size, ordered.size)
+      val newWork = ordered take s
+      val newWorkers = newWork map { step =>
+        Some(step.name -> (time + step.duration))
+      }
+      working ++ newWorkers ++ List.fill(workers.size - s - working.size)(None) -> newWork.toSet
+    }
+    def nextTime: Int =
+      workers.collect {
+        case Some((_, t)) => t
+      }.min
   }
 
   @tailrec
